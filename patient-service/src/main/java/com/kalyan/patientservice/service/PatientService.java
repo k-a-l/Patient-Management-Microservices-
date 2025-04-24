7d136029b5c77aeb1c788f33305f5e221d9b7fd3
Kalyan <kalyankatwal2@gmail.com>
2025-04-24 16:20:22 +0545

    Communication establish between patient and billing Services

--- patient-service/src/main/java/com/kalyan/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/kalyan/patientservice/service/PatientService.java
@@ -4,6 +4,7 @@ import com.kalyan.patientservice.dto.PatientRequestDTO;
 import com.kalyan.patientservice.dto.PatientResponseDTO;
 import com.kalyan.patientservice.exception.EmailAlreadyExistException;
 import com.kalyan.patientservice.exception.PatientNotFoundException;
+import com.kalyan.patientservice.grpc.BillingServiceGrpcClient;
 import com.kalyan.patientservice.mapper.PatientMapper;
 import com.kalyan.patientservice.model.Patient;
 import com.kalyan.patientservice.repository.PatientRepository;
@@ -25,6 +26,9 @@ public class PatientService {
     @Autowired
     private PatientRepository patientRepository;
 
+    @Autowired
+    private BillingServiceGrpcClient billingServiceGrpcClient;
+
     public List<PatientResponseDTO> getPatients() {
         List<Patient> patients = patientRepository.findAll();
         //List<PatientResponseDTO> patientResponseDTOS = patients.stream().map(patient-> PatientMapper.toDTO(patient)).toList();
@@ -37,6 +41,7 @@ public class PatientService {
             throw new EmailAlreadyExistException("This is is already used" + patientRequestDTO.getEmail());
         }
         Patient newPatient = patientRepository.save(PatientMapper.toEntity(patientRequestDTO));
+        billingServiceGrpcClient.createBillingAccount(newPatient.getId().toString(),newPatient.getName(),newPatient.getEmail());
 
         return PatientMapper.toDTO(newPatient);
 
